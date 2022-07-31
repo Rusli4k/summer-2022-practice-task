d5d68ef541a2f9b964a5c5d43a920fec258349d5
rusli4k <rusli@i.ua>
2022-07-31 14:04:14 +0300

    first code

--- main.go
+++ main.go
@@ -1,24 +1,75 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
 type Trains []Train
 
 type Train struct {
-	TrainID            int       
-	DepartureStationID int       
-	ArrivalStationID   int       
-	Price              float32   
-	ArrivalTime        time.Time 
+	TrainID            int
+	DepartureStationID int
+	ArrivalStationID   int
+	Price              float32
+	ArrivalTime        time.Time
 	DepartureTime      time.Time
 }
 
-func main() {
-	//	... запит даних від користувача
-	//result, err := FindTrains(departureStation, arrivalStation, criteria))
-	//	... обробка помилки
-	//	... друк result
+func checkForErrors(err error) {
+	if err != nil {
+		fmt.Println("error is detected:", err)
+	}
 }
 
-func FindTrains(departureStation, arrivalStation, criteria string) (Trains, error) {
-	// ... код
-	return nil, nil // маєте повернути правильні значення
+func main() { //func getTrains
+	jsonfile, err := os.Open("data.json")
+	defer jsonfile.Close()
+	checkForErrors(err)
+	data, err := io.ReadAll(jsonfile)
+	checkForErrors(err)
+	var trains Trains
+	//fmt.Println(string(data))
+	err = json.Unmarshal(data, &trains)
+	checkForErrors(err)
+	for i, v := range trains {
+		fmt.Println(i, v)
+		if i == 5 {
+			break
+		}
+	}
+	// data := make([]byte, 179)
+	// jsonfile.Read(data)
+	// fmt.Println(string(data))
+
+}
+
+func (t *Train) UnmarshalJSON(data []byte) error {
+	type Alias Train
+	aux := &struct {
+		ArrivalTime   string
+		DepartureTime string
+		*Alias
+	}{
+		Alias: (*Alias)(t),
+	}
+	err := json.Unmarshal(data, &aux)
+	if err != nil {
+		return err
+	}
+
+	t.ArrivalTime, err = time.Parse("15:04:05", aux.ArrivalTime)
+	if err != nil {
+		return err
+	}
+
+	t.DepartureTime, err = time.Parse("15:04:05", aux.DepartureTime)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
